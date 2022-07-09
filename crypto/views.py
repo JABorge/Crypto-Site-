379c7ad91f6d575d277b10d1610e7f919ea23206
Jorge Borge <jorgealbertoborge@gmail.com>
2022-07-09 13:05:37 -0400

    Initial commit

---
+++ crypto/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    import requests 
+    import json 
+    api_request = requests.get()
+    api = json.loads(api_request.content)
+    return render(request, 'home.html', {'api': api})
